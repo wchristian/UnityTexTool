c5daf7c69f66f897117fdf43944bc50345e5013d
Christian Walde <walde.christian@gmail.com>
2018-12-11 01:50:38 +0100

    parse tex files emitted by unityex

--- UnityTexTool/UnityEngine/Texture2D.cs
+++ UnityTexTool/UnityEngine/Texture2D.cs
@@ -53,8 +53,8 @@ namespace UnityTexTool.UnityEngine
             {
                 using (EndianBinaryReader br = new EndianBinaryReader(ms , Endian.LittleEndian))
                 {
-                    int name_length = br.ReadInt32();
-                    this.name = Encoding.UTF8.GetString(br.ReadBytes(name_length));
+                    // int name_length = br.ReadInt32();
+                    // this.name = Encoding.UTF8.GetString(br.ReadBytes(name_length));
                     var pos = br.BaseStream.Position;
                     if (pos % 4 != 0)
                     {
